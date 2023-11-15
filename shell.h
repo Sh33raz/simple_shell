785d60cb30e4c501f206eb9d4309472fe84a23fe
Sh33raz <141706084+Sh33raz@users.noreply.github.com>
2023-11-15 17:16:39 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -29,7 +29,7 @@ typedef struct str_list
 } list_me;
 
 /**
- *strust_info - info
+ *struct_info - info
  *@arg: args
  *@argv: array of args
  *@pa: path
@@ -51,25 +51,25 @@ typedef struct str_list
  */
 typedef struct info
 {
-	char *arg;
-	char **argv;
-	char *pa;
-	int argc;
-	unsigned int l_count;
-	int err_num;
-	int l_flag;
-	char *fname;
-	list_me *_env;
-	list_me *history;
-	list_me *alias;
-	char **_environ;
-	int env_ch;
+	char *argu;
+	char **arguv;
+	char *pant;
+	int arguc;
+	unsigned int line_count;
+	int er_num;
+	int line_flag;
+	char *funname;
+	list_me *_envenv;
+	list_me *histo;
+	list_me *aliaz;
+	char **_envrn;
+	int env_me;
 	int status;
 
 	char **buf_me;
 	int buf_me_type;
-	int readfd;
-	int h_count;
+	int readfind;
+	int me_count;
 } me _t;
 
 #define _INFO \
@@ -93,5 +93,69 @@ int hsh(me_t *, char **);
 int find_blt(me_t *);
 void find_words(me_t *);
 void fork_words(me_t *);
+
+/* PATH.c */
+int is_words(me_t *, char *);
+char *duplc(char *, int, int);
+char *path_finder(me_t *, char *, char *);
+
+/* ERR_S.c */
+void _errputs(char *);
+int _errputchar(char);
+int _putfind(char, int);
+int _putsfind(char *, int);
+
+/* STRINGS.c */
+int _len_(char *);
+int _cmp_(char *, char *);
+char *_starts_(const char *, const char *);
+char *_cat_(char *, char *);
+char *_cpy_(char *, char *);
+char *_dup_(const char *);
+void _puts_(char *);
+int _putchar_(char);
+char *_strncpy_(char *, char *, int);
+char *_strncat_(char *, char *, int);
+char *_chara_(char *, char);
+char **_strtow_(char *, char *);
+char **_strtow2_(char *, char);
+
+/* MEMORY.c */
+char *_memoset(char *, char, unsigned int);
+void _fr(char **);
+void *_realloc_(void *, unsigned int, unsigned int);
+int befree(void **);
+
+/* BUILTIN.c */
+int exit_me(me_t *);
+int cd_me(me_t *);
+int help_me(me_t *);
+int history_me(me_t *);
+int unset_me(me_t *, char *);
+int set_me(me_t *, char *);
+int palias(list_t *);
+int alias_me(me_t *);
+
+/* GETLINE.c */
+ssize_t buff_i(me_t *, char **, size_me *);
+ssize_t _nputget(me_t *);
+ssize_t buff_r(me_t *, char *, size_me *);
+int _lineget(my_t *, char **, size_me *);
+void _sigee(int);
+
+/* MY.c module */
+void clear_me(me_t *);
+void set_me(me_t *, char **);
+void free_me(me_t *, int);
+
+/* ENV.c */
+char *envget(me_t *, const char *);
+int env_me(me_t *);
+int setenv_me(me_t *);
+int unsetenv_me(me_t *);
+int _pplate(me_t *);
+char **_environget(me_t *);
+int _unsetenv(me_t *, char *);
+int _setenv(me_t *, char *, char *);
 
 #endif
